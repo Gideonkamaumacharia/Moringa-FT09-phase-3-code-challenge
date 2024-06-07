d6c8c1fb7b82296524e944ba528761d2b7f1bebe
Gideon Macharia <kamaugedion95@gmail.com>
2024-06-07 23:58:38 +0300

    commit

--- models/author.py
+++ models/author.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 class Author:
     def __init__(self, id, name):
         self.id = id
@@ -6,4 +8,14 @@ class Author:
     def __repr__(self):
         return f'<Author {self.name}>'
     
-    
+    def add_to_database(self):
+        conn = sqlite3.connect('./database/magazine.db')
+        cursor = conn.cursor()
+
+        cursor.execute("""INSERT INTO authors (id,name) VALUES (?,?)""",(self.id,self.name))
+        cursor.fetchone()
+
+        conn.commit()
+        conn.close()
+
+new_author = Author(1,"John Doe")
